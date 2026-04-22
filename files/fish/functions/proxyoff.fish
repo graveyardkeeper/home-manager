7208a4fb5fe9ffe383ab76d8f9bf44b02ca16e70
graveyardkeeper <naughty_dog@qq.com>
2026-04-22 17:40:43 +0800

    Remove proxyon/off fish function

--- files/fish/functions/proxyoff.fish
+++
@@ -1,6 +0,0 @@
-function proxyoff
-
-    set -e HTTP_PROXY
-    set -e HTTPS_PROXY
-
-end
--- files/fish/functions/proxyon.fish
+++
@@ -1,6 +0,0 @@
-function proxyon
-
-    set -gx HTTP_PROXY http://127.0.0.1:8080
-    set -gx HTTPS_PROXY http://127.0.0.1:8080
-
-end
